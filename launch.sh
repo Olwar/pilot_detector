c291004a7693be1c2ab836651f7bf86ef202687e
Olli Airola <olli.airola@gmail.com>
2022-12-13 10:36:33 +0200

    adding python3 to the list of dependencies

--- launch.sh
+++ launch.sh
@@ -3,13 +3,14 @@
 # This script is used to launch the application locally
 
 # install all the depencies
+sudo apt install python3
 pip install -r requirements.txt
 
 # run the backend
 cd backend
 python3 pilot_detector.py > /dev/null &
 uvicorn main:app --reload > /dev/null &
-sleep 5
+sleep 3
 
 # run the frontend
 cd ../frontend
@@ -17,7 +18,8 @@ echo "Press that ip-address with port 1337 to see the website"
 python3 -m http.server 1337
 
 echo " "
-echo "Write kill to kill all the processes"
+echo "Write kill to kill all the background processes used for this"
+echo " "
 while :
 do
     read var
